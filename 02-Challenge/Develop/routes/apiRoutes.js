8b83b90d50ff6f399a80ea0a5901f335f5041107
Santy520 <sspalacio20@gmail.com>
2024-04-29 22:07:50 -0400

    pushing

--- 02-Challenge/Develop/routes/apiRoutes.js
+++ 02-Challenge/Develop/routes/apiRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const fs = require ('fs')
+const uuid = require ('../helpers/uuid');
 
 router.get('/notes', (req, res) => {
         fs.readFile('db/db.json', 'utf-8', (err, data) => {
@@ -11,6 +12,24 @@ router.get('/notes', (req, res) => {
         });
 });
 
-router.post
+router.post('/notes', (req, res) => {
+    console.info(`${req.method} request to add a note`);
+    console.log(req.body);
+
+    const { username, topic } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            username,
+            topic,
+            note_id: uuid(),
+        };
+
+        readAndAppend(newNote, '../db/db.json');
+        res.json('note added succesfully');
+    } else {
+        res.error('Error in adding note');
+    }
+});
 
 module.exports = router
